ace39df8e9c45d3827c66a50e39b9ff952e1f8df
BeckReed <beckreednote@outlook.com>
2017-04-09 01:06:39 +0800

    requirejs初始化

---
+++ js/base.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Beck on 4/9/2017.
+ */
+
+;define(['jquery'],function($){
+    console.log('aaaa');
+
+});
---
+++ js/lib/requirejs/require.config.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Beck on 4/9/2017.
+ */
+
+var require={
+    baseUrl:'../js',
+    paths : {
+        "jquery" : ["http://libs.baidu.com/jquery/2.0.3/jquery-test-NoExist","lib/jquery/jquery-1.12.4"],
+        //"a" : "/WebStudy/js/a"
+    },
+    //非AMD模块加载配置
+    shim:{
+        "underscore":{
+            exports:"_"
+        },
+        "jquery.form":{
+            deps:["jquery"]
+        }
+        /*
+         可以简写：
+         "jquery.form":["jquery"]
+         * */
+    }
+};
+/*var require=require.config({
+    baseUrl:'../js',
+    paths : {
+        "jquery" : ["http://libs.baidu.com/jquery/2.0.3/jquery-test-NoExist","lib/jquery/jquery-1.12.4"],
+        //"a" : "/WebStudy/js/a"
+    },
+    //非AMD模块加载配置
+    shim:{
+        "underscore":{
+            exports:"_"
+        },
+        "jquery.form":{
+            deps:["jquery"]
+        }
+        /!*
+         可以简写：
+         "jquery.form":["jquery"]
+         * *!/
+    }
+})*/
